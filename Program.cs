2ac02574b59af6d48748f12cf0780d32565ad7f3
Matheos Mattsson <matheos.mattsson@gmail.com>
2025-07-10 11:10:42 +0300

    Implement IConfiguration and use-cases Added appsettings.json and a few settings which the user can configure to their liking to control how the application behaves.

--- Program.cs
+++ Program.cs
@@ -2,15 +2,27 @@
 
 using System;
 using Avalonia;
+using Microsoft.Extensions.Configuration;
 
 class Program
 {
+    public static AppSettings AppSettings { get; private set; } = null!;
+    
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional:true)
+            .Build();
+        
+        AppSettings = configuration.GetSection("AppSettings").Get<AppSettings>() ?? new AppSettings();
+        
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
@@ -18,4 +30,11 @@ class Program
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
+}
+
+internal class AppSettings
+{
+    public bool ReadExif { get; init; } = true;
+    public bool ShowSelectedFilesList { get; init; } = true;
+    public int ThumbnailSize { get; init; } = 300; // Default thumbnail size is 300 pixels
 }
